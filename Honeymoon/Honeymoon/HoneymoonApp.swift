7a51b2a4f9ca13cd751c113b0da03f38edf8de65
gurjnderSingh <gurjindersingh663@gmail.com>
2023-03-17 22:52:58 +0530

    project created

---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by Gurjinder Singh on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
