a82a4faab7a90dfa3efcef4c3e24ef36aa426dfd
gurjnderSingh <gurjindersingh663@gmail.com>
2023-03-17 23:02:04 +0530

    model and data file added

---
+++ Honeymoon/Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,13 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Gurjinder Singh on 17/03/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Destination {
+    var place, country, image: String
+}
